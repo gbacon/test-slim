25e010a44c6dea32450e3c8d2191f78ac49c4662
Greg Bacon <gbacon@gbacon.com>
2011-01-06 19:46:39 -0600

    FIXME

---
+++ fixtures/EchoSupport.pm
@@ -0,0 +1,13 @@
+package EchoSupport;
+
+use strict;
+use warnings;
+
+sub new {
+  my($class) = @_;
+  bless {} => $class;
+}
+
+sub echo {}
+
+1;
---
+++ fixtures/LibraryFixture.pm
@@ -0,0 +1,8 @@
+package LibraryFixture;
+
+use strict;
+use warnings;
+
+sub new {}
+
+1;
